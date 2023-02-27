b521e4af22c9f0284971cfa20d7e81bb005e536d
Allan Oricil <allanoricilcos@outlook.com>
2023-02-26 23:58:01 -0300

    remove @rollup/plugin-terser

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,3 @@
-import terser from "@rollup/plugin-terser";
 import commonjs from "@rollup/plugin-commonjs";
 import nodeResolve from "@rollup/plugin-node-resolve";
 import babel from "@rollup/plugin-babel";
